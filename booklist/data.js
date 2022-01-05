6cb187dfd1b1481fe8b12174cd2ec52228a5fc73
Frostypine <frostypinekng@yahoo.com>
2022-01-05 14:43:08 -0700

    worked on controllers more

---
+++ booklist/data.js
@@ -0,0 +1,10 @@
+const data = 
+[
+{id: 0, title: "To Kill a Mockingbird", author: "Harper Lee", synopsis: "Boo, Scout and Gregory play in a treehouse."},
+{id: 1, title: "Moby Dick", author: "Herman Melville", synopsis: "the white whale"},
+{id: 2, title: "the Great Gatsby", author: "F. Scott Fitzgerald", synopsis: "I prefer zelda"},
+
+
+]
+
+export default  data; 
