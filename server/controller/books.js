527e991d6d0c7e3d0909bb7582e8478d1e2131e8
Frostypine <frostypinekng@yahoo.com>
2022-01-05 14:12:31 -0700

    working on controllers and a little redux

---
+++ server/controller/books.js
@@ -0,0 +1,17 @@
+//controller/books.js
+import mongoose from 'mongoose';
+import Books from '../models/books.js'; 
+
+// set up the getBooks and addBook exports
+
+export const getBooks = async (req, res) => 
+{
+
+}
+
+export const addBook = async (req, res) =>
+{
+
+}
+
+
