2879d98fd987ab04623f2c3340d3cc0a249bea19
Frostypine <frostypinekng@yahoo.com>
2022-01-13 10:56:06 -0700

    working on socket

--- booklist/server/index.js
+++ booklist/server/index.js
@@ -1,29 +1,39 @@
 // server/index.js
-
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors'; 
-
 import bookRoutes from './routes/books.js'; 
 import testRoutes from './routes/test.js';
 
 const app = express();
 const PORT = '5000'; 
   
-
-
   // set up middle wares 
   app.use(express.json());
   app.use(express.urlencoded());
   app.use(cors());
+
+ //socket
+ import {Server} from 'socket.io'; 
+ import {createServer} from 'http'; 
+ const httpServer = createServer(app); 
+ const io = new Server(httpServer, {
+    cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+ }); 
+ 
+ io.on("connection", (socket) => {
+   console.log("user connected");
+ })
 
   //set up route
   app.use('/books', bookRoutes);
   app.use('/test', testRoutes);
 
   //set up mongodb connection 
-
-const CONNECTION_URL = 'mongodb://localhost:27017/test'; 
+const CONNECTION_URL = 'mongodb://localhost:27017/booklist'; 
   mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
   .catch((err) => console.log(err.message));
